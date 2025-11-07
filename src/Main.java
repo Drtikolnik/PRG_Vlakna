5d2c761211b8c38b4cbdcb0e3be7a865ab5cb748
drtluk00 <drtluk00@gasos-ro.cz>
2025-11-07 08:11:15 +0100

    uwu

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+import java.util.InputMismatchException;
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws InterruptedException {
+
+//1----------
+    /*
+        Thread vlakno11 = new Thread(()->{
+            for(int i = 0; i < 1000; i++) {
+                System.out.println(i);
+            }
+        });
+
+        Thread vlakno21 = new Thread(()->{
+            for(char i = 'a'; i <= 'z'; i++) {
+                System.out.println(i);
+            }
+        });
+
+        vlakno11.start();
+        vlakno21.start();
+    */
+
+//2----------
+    /*
+        Thread spiciVlakno = new Thread(()->{
+
+                System.out.println(Thread.currentThread().getName()); ;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("vlákno " +Thread.currentThread().getName()+ " uběhlo");
+
+        });
+        spiciVlakno.setName("Spícivlákno");
+
+        spiciVlakno.start();
+
+        Thread.sleep(1000);
+
+        spiciVlakno.interrupt();
+    */
+
+//2----------
+        Thread zdravitko = new Thread(()->{
+            if(Thread.currentThread().isInterrupted() == false) {
+                for(;;){
+                    System.out.println("Ahoj");
+                }
+            }
+
+        });
+        zdravitko.setName("Zdravítko");
+
+
+        Thread stopka = new Thread(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {}
+            System.out.println("STAČILO!");
+            zdravitko.interrupt();
+
+        });
+        stopka.setName("Stopka");
+
+        zdravitko.start();
+        stopka.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
